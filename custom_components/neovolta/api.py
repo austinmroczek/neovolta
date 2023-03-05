5e8df4c03a091202b3de89c2c089d4ef25ffea28
Austin Mroczek <austin@mroczek.org>
2023-03-05 18:15:11 +0000

    updated code

--- custom_components/neovolta/api.py
+++ custom_components/neovolta/api.py
@@ -50,29 +50,73 @@ class NeovoltaApiClient:
 
         self._static_data_loaded = True
 
+    def _scaled_value(self, value, scale=1) -> any:
+        """Return a scaled value."""
+        return float(value) * scale
+
     async def async_get_data(self) -> any:
         """Get data from the API."""
         if not self._static_data_loaded:
             await self._async_get_static_data()
 
         _LOGGER.debug("Neovolta starting async_get_data")
-        # battery total %
-        response = await self._get_value(184, 1)
-        self.data["battery_total"] = response[0]
-
-        # battery component TBD %
-        response = await self._get_value(316, 1)
-        self.data["battery_tbd"] = response[0]
 
         # battery enegery today
-        response = await self._get_value(70, 8)
-        self.data["battery_charged_today"] = float(response[0]) * 0.1
-        self.data["battery_discharged_today"] = float(response[1]) * 0.1
-        self.data["battery_charged_cummulative"] = float(response[2]) * 0.1
+        response = await self._get_value(70, 10)
+        self.data["battery_charged_today"] = self._scaled_value(response[0], 0.1)
+        self.data["battery_discharged_today"] = self._scaled_value(response[1], 0.1)
+        self.data["battery_charged_cummulative"] = self._scaled_value(response[2], 0.1)
         # unusre if response[3] is anything useful
-        self.data["battery_discharged_cummulative"] = float(response[4]) * 0.1
-        self.data["energy_from_grid_today"] = float(response[6]) * 0.1
-        self.data["energy_to_grid_today"] = float(response[7]) * 0.1
+        self.data["battery_discharged_cummulative"] = self._scaled_value(
+            response[4], 0.1
+        )
+        self.data["energy_from_grid_today"] = self._scaled_value(response[6], 0.1)
+        self.data["energy_to_grid_today"] = self._scaled_value(response[7], 0.1)
+        # 79: frequency TBD1
+        self.data["frequency1"] = self._scaled_value(response[9], 0.01)
+
+        response = await self._get_value(81, 5)
+        # 81: energy to grid cummulative
+        self.data["energy_to_grid_cummulative"] = self._scaled_value(response[0], 0.1)
+        # 84: energy consumed today
+        self.data["energy_consumed_today"] = self._scaled_value(response[3], 0.1)
+        # 85: energy consumed cummulative
+        self.data["energy_consumed_cummulative"] = self._scaled_value(response[4], 0.1)
+
+        response = await self._get_value(126, 1)
+        # 126: battery voltage
+        self.data["battery_voltage1"] = self._scaled_value(response[0], 0.01)
+
+        response = await self._get_value(138, 3)
+        # 138: grid voltage R/U/A
+        self.data["grid_voltage_rua"] = self._scaled_value(response[0], 0.1)
+        # 139: grid voltage S/V/B
+        self.data["grid_voltage_svb"] = self._scaled_value(response[1], 0.1)
+        # 140: grid voltage RS/UV/AB
+        self.data["grid_voltage_rsuvab"] = self._scaled_value(response[2], 0.1)
+
+        response = await self._get_value(143, 1)
+        # 143: battery voltage
+        self.data["battery_voltage2"] = self._scaled_value(response[0], 0.01)
+
+        response = await self._get_value(183, 2)
+        # 183: battery voltage
+        self.data["battery_voltage3"] = self._scaled_value(response[0], 0.01)
+        # 184: battery total %
+        self.data["battery_total"] = self._scaled_value(response[1], 1)
+
+        response = await self._get_value(192, 2)
+        # 192: frequency TBD
+        self.data["frequency2"] = self._scaled_value(response[0], 0.01)
+        # 193: frequency TBD
+        self.data["frequency3"] = self._scaled_value(response[1], 0.01)
+
+        response = await self._get_value(316, 2)
+        # 316: battery component TBD %
+        self.data["battery_tbd"] = self._scaled_value(response[0], 1)
+        # 317: battery voltage
+        self.data["battery_voltage4"] = self._scaled_value(response[1], 0.01)
+
         _LOGGER.debug("Neovolta finish async_get_data")
 
     async def _get_value(
@@ -80,7 +124,7 @@ class NeovoltaApiClient:
         address: int,
         size: int,
         unit: int = 1,
-        tries=5,
+        tries=10,
     ) -> any:
         """Get information from the API."""
         try:
